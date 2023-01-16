90d63d7f4a91d407c3331a09cf1ff863465a0734
audreyscheah <122134118+audreyscheah@users.noreply.github.com>
2023-01-15 20:19:54 -0500

    Upgrade Cooper Site

---
+++ index.js
@@ -0,0 +1,13 @@
+let x = 5;
+let y = 7;
+
+let answer = x + y;
+
+console.log(answer);
+
+$("#pressme").click(sayhi);
+
+function sayhi() {
+  let username = $("#users-name").val();
+  console.log(username);
+}
